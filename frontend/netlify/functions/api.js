0405e5d4bd1e83ad30fa5a84ece6aa6c3935dbc9
AdrianaMHernandez <nanamarcela109@gmail.com>
2026-09-21 17:33:22 -0500

    fix: multi-fallback require in frontend function

--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -1,6 +1,18 @@
 const serverless = require('serverless-http');
-const { app } = require('../../../backend/app');
 
-const handler = serverless(app);
+let appModule;
+try {
+  appModule = require('../../../backend/app');
+} catch (e) {
+  try {
+    appModule = require('../../backend/app');
+  } catch (e2) {
+    const path = require('path');
+    appModule = require(path.resolve(__dirname, '../../../backend/app'));
+  }
+}
 
+const handler = serverless(appModule.app);
+
+module.exports = { handler };
 exports.handler = handler;
